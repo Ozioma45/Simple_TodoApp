a711ff2330a6e9b9c96c114e302ec4154eefac13
ozioma45 <oziomaegole@gmail.com>
2024-02-21 14:54:44 +0100

    add styling to the tasks

--- script.js
+++ script.js
@@ -4,11 +4,38 @@ function addTask() {
 
   if (taskInput.value.trim() !== "") {
     var li = document.createElement("li");
+    var ctrl = document.createElement("div");
+
+    // Add classes to the control div
+    ctrl.classList.add("ctrl");
+
+    // Set innerHTML of the li element
+    li.classList.add(
+      "bg-white",
+      "col-md-8",
+      "tasks",
+      "col-12",
+      "text-start",
+      "ps-3",
+      "d-flex",
+      "align-items-center"
+    );
     li.innerHTML = '<span class="taskText">' + taskInput.value + "</span>";
-    li.innerHTML += '<span class="edit" onclick="editTask(this)">✎</span>';
-    li.innerHTML += '<span class="delete" onclick="deleteTask(this)">❌</span>';
-    li.innerHTML += '<input type="checkbox" onclick="toggleTaskStatus(this)">';
+
+    // Move the controls to the control div
+    ctrl.innerHTML += '<span class="edit" onclick="editTask(this)">✎</span>';
+    ctrl.innerHTML +=
+      '<span class="delete" onclick="deleteTask(this)">❌</span>';
+    ctrl.innerHTML +=
+      '<input type="checkbox" onclick="toggleTaskStatus(this)">';
+
+    // Append the control div to the list item
+    li.appendChild(ctrl);
+
+    // Append the list item to the task list
     taskList.appendChild(li);
+
+    // Clear the input field
     taskInput.value = "";
   } else {
     alert("Please enter a task!");
@@ -16,11 +43,11 @@ function addTask() {
 }
 
 function deleteTask(task) {
-  task.parentNode.remove();
+  task.parentNode.parentNode.remove(); // Remove the parent of the parent, i.e., the li element
 }
 
 function editTask(task) {
-  var listItem = task.parentNode;
+  var listItem = task.parentNode.parentNode; // Get the parent of the parent, i.e., the li element
   var taskText = listItem.querySelector(".taskText");
   var newText = prompt("Edit task:", taskText.textContent);
 
@@ -30,7 +57,7 @@ function editTask(task) {
 }
 
 function toggleTaskStatus(checkbox) {
-  var listItem = checkbox.parentNode;
+  var listItem = checkbox.parentNode.parentNode; // Get the parent of the parent, i.e., the li element
   var taskText = listItem.querySelector(".taskText");
 
   if (checkbox.checked) {
